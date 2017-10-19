820f2c289a3039536884a34b93b0b04c4cb8fff0
Peter <ptrmelko@gmail.com>
2017-10-19 18:56:15 -0400

    CSS Updates

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,13 @@ var gulp = require('gulp');
 
 // Include Plugins
 var watch = require('gulp-watch');
+
+// CSS Plugins
 var postCSS = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var CSSvars = require('postcss-simple-vars');
 var nestedCSS = require('postcss-nested');
+var importCSS = require('postcss-import');
 
 // gulp default task
 gulp.task('default', function() {
@@ -19,7 +22,8 @@ gulp.task('html', function() {
 // CSS Post-processing
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postCSS([nestedCSS, CSSvars, autoprefixer]))
+  //  .pipe(postCSS([nestedCSS, CSSvars, autoprefixer, importCSS]))
+    .pipe(postCSS([importCSS, CSSvars, nestedCSS, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
 });
 
