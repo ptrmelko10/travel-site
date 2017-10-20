160758b9c56007dc478067f03517d8c885bda65e
Peter <ptrmelko@gmail.com>
2017-10-19 20:56:02 -0400

    Begin Responsive work

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -7,12 +7,13 @@ var autoprefixer = require('autoprefixer');
 var CSSvars = require('postcss-simple-vars');
 var nestedCSS = require('postcss-nested');
 var importCSS = require('postcss-import');
+var mixins = require('postcss-mixins');
 
 // CSS Post-processing
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
   //  .pipe(postCSS([nestedCSS, CSSvars, autoprefixer, importCSS]))  Order does matter!!!
-    .pipe(postCSS([importCSS, CSSvars, nestedCSS, autoprefixer]))
+    .pipe(postCSS([importCSS, mixins, CSSvars, nestedCSS, autoprefixer]))
     .on('error', function (errorInfo) {
       console.log(errorInfo.toString());
       this.emit('end');
