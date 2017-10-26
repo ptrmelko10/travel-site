379d6077f366ccbb10bee5af6e63872fab005fab
Peter <ptrmelko@gmail.com>
2017-10-26 17:52:44 -0400

    Installed Babel and init setup

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -67,30 +67,92 @@
 /* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
-var Personnn = __webpack_require__(1);
+"use strict";
+
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _Person2 = __webpack_require__(1);
+
+var _Person3 = _interopRequireDefault(_Person2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 var $ = __webpack_require__(2);
 
-var john = new Person("John Doe", "blue");
-var jane = new Person("Jane Smith", "green");
+//ES6 way to import a module
+
+var Adult = function (_Person) {
+  _inherits(Adult, _Person);
+
+  function Adult() {
+    _classCallCheck(this, Adult);
+
+    return _possibleConstructorReturn(this, (Adult.__proto__ || Object.getPrototypeOf(Adult)).apply(this, arguments));
+  }
+
+  _createClass(Adult, [{
+    key: 'payTaxes',
+    value: function payTaxes() {
+      console.log(this.name + ' now owes $0 in taxes.');
+    }
+  }]);
+
+  return Adult;
+}(_Person3.default);
+
+var john = new _Person3.default("John Doe", "blue");
+var jane = new Adult("Jane Smith", "orange");
 
 john.greet();
 jane.greet();
 
+jane.payTaxes();
 
 /***/ }),
 /* 1 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-function Person (name, color) {
-  this.name = name;
-  this.color = color;
-  this.greet = function () {
-    console.log("Hello, my name is " + this.name + " and my favorite color is " + this.color + ".");
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Person = function () {
+  function Person(name, color) {
+    _classCallCheck(this, Person);
+
+    this.name = name;
+    this.color = color;
   }
-}
 
-module.exports = Person;
+  _createClass(Person, [{
+    key: "greet",
+    value: function greet() {
+      console.log("Hello, my name is " + this.name + " and my favorite color is " + this.color + ".");
+    }
+  }]);
 
+  return Person;
+}();
+
+//module.exports = Person;
+//ES 6 way to export a class
+
+
+exports.default = Person;
 
 /***/ }),
 /* 2 */
