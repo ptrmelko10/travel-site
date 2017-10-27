5fa8e5ea6ba3a6a35332a813f829bb36506b4eb7
Peter <ptrmelko@gmail.com>
2017-10-27 04:46:19 -0400

    Completed Reveal on Scroll visual

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,7 @@
+import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 var mobileMenu = new MobileMenu();
+var revealOnScrollFeature = new RevealOnScroll($('.feature-item'), '85%');
+var revealOnScrollTestimonial = new RevealOnScroll($('.testimonial'), '85%');
