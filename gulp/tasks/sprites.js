cce19c9905343c3db841b741ca23daf3f8b3db78
Peter <ptrmelko@gmail.com>
2017-10-28 07:27:16 -0400

    Completed old browser support

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -5,11 +5,24 @@ var gulp = require('gulp');
 var svgSprite = require('gulp-svg-sprite');
 var rename = require('gulp-rename');
 var del = require('del');
+var svg2png = require('gulp-svg2png');
 
 //  SVG Sprite Configuration
 var config = {
+  shape: {
+    spacing: {
+      padding: 1
+    }
+  },
   mode : {
     css: {
+      variables: {
+        replaceSvgWithPng: function() {
+          return function(sprite, render) {
+            return render(sprite).split('.svg').join('.png');
+          }
+        }
+      },
       sprite: 'sprite.svg',
       render: {
         css: {
@@ -30,8 +43,14 @@ gulp.task('createSprite', ['beginClean'],function() {
     .pipe(gulp.dest('./app/temp/sprite/'));
 });
 
-gulp.task('copySpriteGraphic', ['createSprite'], function() {
-  return gulp.src('./app/temp/sprite/css/**/*.svg')
+gulp.task('createPngCopy', ['createSprite'],function () {
+  return gulp.src('./app/temp/sprite/css/*.svg')
+    .pipe(svg2png())
+    .pipe(gulp.dest('./app/temp/sprite/css'));
+});
+
+gulp.task('copySpriteGraphic', ['createPngCopy'], function() {
+  return gulp.src('./app/temp/sprite/css/**/*.{svg,png}')
     .pipe(gulp.dest('./app/assets/images/sprites'));
 });
 
@@ -45,4 +64,4 @@ gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'],function () {
   return del('./app/temp/sprite');
 });
 
-gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
+gulp.task('icons', ['beginClean', 'createSprite', 'createPngCopy', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
